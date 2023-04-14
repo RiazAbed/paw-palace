0aba92c97ed458cb9dbde75e7d9812a4f96ae256
Riaz Abed <r.abed@portofrotterdam.com>
2023-04-14 10:32:11 +0200

    first commit

---
+++ src/main/java/com/booking/animal/pawpalace/PawPalaceApplication.java
@@ -0,0 +1,13 @@
+package com.booking.animal.pawpalace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawPalaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawPalaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booking/animal/pawpalace/PawPalaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.animal.pawpalace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawPalaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
